01f5b8ae0fe51ebcc85cb0fbafb640a0c5eb58a2
Fate <bboyfate@163.com>
2019-03-20 20:19:08 +0800

    reply delete

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -16,10 +16,55 @@ export default class ReplyMimin extends wepy.mixin {
     // 当前页数
     page: 1
   }
+  methods = {
+    // 删除回复
+    async deleteReply (topicId, replyId) {
+      let res = await wepy.showModal({
+        title: '确认删除',
+        content: '您确认删除该回复吗',
+        confirmText: '删除',
+        cancelText: '取消'
+      })
+
+      // 点击取消后返回
+      if (!res.confirm) {
+        return
+      }
+
+      try {
+        // 调用接口删除回复
+        let deleteResponse = await api.authRequest({
+          url: 'topics/' + topicId + '/replies/' + replyId,
+          method: 'DELETE'
+        })
+
+        // 删除成功
+        if (deleteResponse.statusCode === 204) {
+          wepy.showToast({
+            title: '删除成功',
+            icon: 'success',
+            duration: 2000
+          })
+          // 将删除了的回复移除
+          this.replies = this.replies.filter((reply) => reply.id !== replyId)
+          this.$apply()
+        }
+
+        return deleteResponse
+      } catch (err) {
+        console.log(err)
+        wepy.showModal({
+          title: '提示',
+          content: '服务器错误，请联系管理员'
+        })
+      }
+    }
+  }
 
   // 获取话题回复
   async getReplies (reset = false) {
     try {
+      let that = this
       // 请求话题回复接口
       let repliesResponse = await api.request({
         url: this.requestData.url,
@@ -31,9 +76,11 @@ export default class ReplyMimin extends wepy.mixin {
 
       if (repliesResponse.statusCode === 200) {
         let replies = repliesResponse.data.data
-
+        let user = await this.$parent.getCurrentUser()
         // 格式化回复的创建时间
         replies.forEach(function (reply) {
+          // 控制是否可以删除
+          reply.can_delete = that.canDelete(user, reply)
           reply.created_at_diff = util.diffForHumans(reply.created_at)
         })
         // 如果 reset 不为 ture 则合并 this.replies；否则直接覆盖
@@ -58,6 +105,15 @@ export default class ReplyMimin extends wepy.mixin {
     }
   }
 
+  // 是否可以删除
+  canDelete (user, reply) {
+    if (!user) {
+      return false
+    }
+
+    return (reply.user_id === user.id)
+  }
+
   // 下拉刷新
   async onPullDownRefresh() {
     this.noMoreData = false
@@ -72,7 +128,6 @@ export default class ReplyMimin extends wepy.mixin {
     if (this.noMoreData || this.isLoading) {
       return
     }
-    console.log(this)
     this.isLoading = true
     this.page = this.page + 1
     await this.getReplies()
